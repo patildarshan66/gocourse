f668b73e381c272da6cc7187f75f583fb1439412
Darshan Patil <darshan.patil@bandhanbank.com>
2025-08-29 20:35:36 +0530

    constants | data types | variables | arithmetic operations

---
+++ gocourse/basics/arithmetic_operations.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+
+	var a, b int = 10, 3
+	var c, d float64 = 10, 3.0
+
+	var result int
+	var resultFloat float64
+
+	// Addition
+	result = a + b
+	fmt.Println("Addition:", result)
+
+	// Subtraction
+	result = a - b
+	fmt.Println("Subtraction:", result)
+
+	// Multiplication
+	result = a * b
+	fmt.Println("Multiplication:", result)
+
+	result = a % b
+	fmt.Println("Modulus:", result)
+
+	// Division
+	resultFloat = c / d
+	fmt.Println("Division:", resultFloat)
+
+	// Overflow with signed integers
+	var maxInt int64 = math.MaxInt64 //9223372036854775807
+	fmt.Println("Max Int64:", maxInt)
+	maxInt = maxInt + 1
+	fmt.Println("Overflowed Int64:", maxInt)
+
+	// Overflow with unsigned integers
+	var maxUint uint64 = math.MaxUint64 // 18446744073709551615
+	fmt.Println("Max Uint64:", maxUint)
+	maxUint = maxUint + 1
+	fmt.Println("Overflowed Uint64:", maxUint)
+
+	// Underflow with signed integers
+	var minFloat64 float64 = math.SmallestNonzeroFloat64 //5e-324
+	fmt.Println("Min float64:", minFloat64)
+	minFloat64 = minFloat64 / math.MaxFloat64
+	fmt.Println("Underflowed float64:", minFloat64)
+
+}
---
+++ gocourse/basics/constants.go
@@ -0,0 +1,18 @@
+package basics
+
+import "fmt"
+
+const PI = 3.14
+const GRAVITY float32 = 9.81
+
+const (
+	SUNDAY      = 0
+	MONDAY  int = 1
+	TUESDAY     = 2.0
+)
+
+func main() {
+	fmt.Println(SUNDAY)
+	fmt.Println(MONDAY)
+	fmt.Println(TUESDAY)
+}
